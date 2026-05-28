948aca68bc6f84f241eda7c6d32f0aca6e985e38
meghajagtap-tech <meghajagtap170@gmail.com>
2026-05-28 12:02:46 +0530

    Initial commit: added index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo 'Hello, World!';
+echo 'This is a simple PHP script.';        
+    if (2==2) {
+        echo 'This condition is true.';
+    } else {
+        echo 'This condition is false.';
+    }
+?>
+Goodbye, World!
